282c899d800a94c6ec9014a612360f8b1e840d13
DontNameMe <emilychestertonhunt@gmail.com>
2024-12-09 17:46:37 +0000

    Day 1 part 2 solution

--- 2024/Day1/Day1.py
+++ 2024/Day1/Day1.py
@@ -4,18 +4,28 @@ import re;
 
 # Part 1 list distance:
 # Calculate the distance between the two lists
-inputFile = open("input");
-inputText = inputFile.read();
-inputList = re.split("\n|   ",inputText);
+def generateLists():
+    inputFile = open("input");
+    inputText = inputFile.read();
+    inputList = re.split("\n|   ",inputText);
 
-locIDList1 = inputList[::2]
-locIDList2 = inputList[1::2]
+    locIDList1 = inputList[::2]
+    locIDList2 = inputList[1::2]
+    return locIDList1, locIDList2;
 
+locIDList1, locIDList2 = generateLists();
 totalDistance = 0
 locIDList1.sort();
 locIDList2.sort();
-differenceList = [abs(int(locIDList1[i]) - int(locIDList2[i])) for i in range(len(locIDList1))];
-differnce = sum(differenceList)
-print("distance : ", differnce);
+distanceList = [abs(int(locIDList1[i]) - int(locIDList2[i])) for i in range(len(locIDList1))];
+distance = sum(distanceList)
+print("distance : ", distance);
 
 # Part 2
+# Find the similarity of the list:
+# Multiply every number in list 1 by the number of times it occurs
+list1, list2 = generateLists();
+similarity = 0;
+similarityList = [int(index) * list2.count(index) for index in list1];
+similarity = sum(similarityList);
+print("similarity : ", similarity);
