d31654f273edaae31c6d82aafd1d169996e27ca9
DontNameMe <emilychestertonhunt@gmail.com>
2024-12-12 15:35:32 +0000

    Day 3 task 2

--- 2024/Day3/Day3Task2.py
+++ 2024/Day3/Day3Task2.py
@@ -1,8 +1,8 @@
-# Day 3 task 1 of the 2024 advent of code programming puzzles
+# Day 3 task 2 of the 2024 advent of code programming puzzles
 # @author Emily Chesterton-Hunt
 #
-# Task1 requires a "corrupted" text file be read and from it we must find uncorrupted mul functions
-# In the format mul(x,y) which we then calculate and add to a total
+# Task2 follows on from task1 with the addition of do() and don't()
+# Which enable and disable mul() functions
 def readFile():
     inputFile = open("input");
     inputText = inputFile.read();
@@ -11,26 +11,47 @@ def readFile():
 
 # Searches the string for all cases where there are mul functions and stores the index in a list
 #
-# @params corrupted string
+# @params corrupted string, functions to search for
 # @returns array of indexes
-def findMuls(inputText):
-    indexOfMul = 0;
+def findFunc(inputText, funcToSearch):
+    index_of_mul = 0;
     indexToSearch = 0;
-    mulStarts = [];
-    while not indexOfMul == -1:
-        indexOfMul = inputText.find("mul(", indexToSearch);
-        indexToSearch = indexOfMul + 1
+    funcStarts = [];
+    while not index_of_mul == -1:
+        index_of_mul = inputText.find(funcToSearch, indexToSearch);
+        indexToSearch = index_of_mul + 1
 
-        if indexOfMul != -1:
-            mulStarts.append(indexOfMul);
-    return mulStarts;
+        if index_of_mul != -1:
+            funcStarts.append(index_of_mul);
+    return funcStarts;
+
+# Finds the state of the mult function by finding whether it is closer to a do or dont function
+#
+# Parameters Index of the current mult function, list of all unseen dos, list of all unseen donts
+# Returns whether the boolean is true
+def checkState(curIndex, doStarts, dontStarts):
+
+    if len(dontStarts) == 0:
+        return True;
+    if len(doStarts) == 0:
+        return False;
+    doDistance = curIndex - int(doStarts[0])
+    dontDistance = curIndex - int(dontStarts[0])
+    if dontDistance > doDistance > 0 or dontDistance < 0:
+        if (dontDistance > 0):
+            del dontStarts[0];
+        return True;
+    else:
+        if (doDistance > 0):
+            del doStarts[0];
+        return False;
 
 
 # Checks for any corruption in the function
 #
 # @params corrupted string, list of function indexes
 # @return 2d array of integers from uncorrupted string
-def checkForErrors(inputText, mulStarts):
+def checkForErrors(inputText, mulStarts, dostarts, dontStarts):
     products = []
 
     for mulStart in mulStarts:
@@ -38,8 +59,9 @@ def checkForErrors(inputText, mulStarts):
         reachedEnd = False;
         numbersToMultiply = [""]
         curIndex = mulStart + 4;
-
-        if not curIndex > len(inputText):
+        enabled = checkState(mulStart, dostarts, dontStarts);
+        print(enabled);
+        if not curIndex > len(inputText) and enabled:
             while not errorHasOccurred and not reachedEnd:
                 if inputText[curIndex] == ")":
                     if len(numbersToMultiply) > 1:
@@ -65,7 +87,7 @@ def checkForErrors(inputText, mulStarts):
     return products;
 
 
-def calculateTotal():
+def calculateTotal(extractedNumbers):
     total = 0;
     for nums in extractedNumbers:
         total += multiply(nums);
@@ -78,7 +100,12 @@ def multiply(nums):
 
 if __name__ == '__main__':
     inputText = readFile();
-    mulStarts = findMuls(inputText);
-    extractedNumbers = checkForErrors(inputText, mulStarts)
+
+    mulStarts = findFunc(inputText, "mul(");
+    doStarts= (findFunc(inputText, "do()"));
+    doStarts.insert(0, 0);
+    dontStarts = findFunc(inputText, "don't()");
+
+    extractedNumbers = checkForErrors(inputText, mulStarts, doStarts, dontStarts)
     print(extractedNumbers);
-    print(calculateTotal());
+    print(calculateTotal(extractedNumbers));
